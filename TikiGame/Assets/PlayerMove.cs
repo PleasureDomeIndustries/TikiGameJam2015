96440f0cc7ea1a4804e81527a6432940f7e25b05
whsanders <whsanders@gmail.com>
2015-08-01 12:04:22 -0400

    Basic player movement

--- TikiGame/Assets/PlayerMove.cs
+++ TikiGame/Assets/PlayerMove.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class PlayerMove : NetworkBehaviour {
 
-	public float p1_speed = 0.5f;
+	public float p1_speed = 0.1f;
 	public KeyCode p1_up = KeyCode.W;
 	public KeyCode p1_down = KeyCode.S;
 	public KeyCode p1_left = KeyCode.A;
@@ -21,8 +21,19 @@ public class PlayerMove : NetworkBehaviour {
 	}
 
 	void FixedUpdate () {
-		if (Input.GetKeyDown (p1_up)) {
-//			GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(trans
+		Vector2 pos = (Vector2)this.transform.position;
+
+		if (Input.GetKey (p1_up)) {
+			GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(pos, pos + Vector2.up, p1_speed));
+		}
+		if (Input.GetKey (p1_down)) {
+			GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(pos, pos + Vector2.down, p1_speed));
+		}
+		if (Input.GetKey (p1_left)) {
+			GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(pos, pos + Vector2.left, p1_speed));
+		}
+		if (Input.GetKey (p1_right)) {
+			GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(pos, pos + Vector2.right, p1_speed));
 		}
 	}
 }
