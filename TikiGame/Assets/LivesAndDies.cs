87952b19c62211b4aff836983c952e177443a36e
whsanders <whsanders@gmail.com>
2015-08-01 18:13:44 -0400

    added script for life and death

---
+++ TikiGame/Assets/LivesAndDies.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LivesAndDies : MonoBehaviour {
+
+	public int Health;
+	public bool DisplayHealthBar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TakeDamage(int damage) {
+		Health -= damage;
+		CheckHealth ();
+	}
+
+	void CheckHealth() {
+		if (Health < 0) Die();
+	}
+
+	void Die() {
+		Destroy (gameObject);
+	}
+}
