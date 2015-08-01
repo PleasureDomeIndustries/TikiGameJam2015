b9e0f6be9763ff8a9934a51cb867fcbbbd1938c4
whsanders <whsanders@gmail.com>
2015-08-01 11:49:54 -0400

    Moved scene

--- TikiGame/Assets/PlayerMove.cs
+++ TikiGame/Assets/PlayerMove.cs
@@ -4,6 +4,12 @@ using System.Collections;
 
 public class PlayerMove : NetworkBehaviour {
 
+	public float p1_speed = 0.5f;
+	public KeyCode p1_up = KeyCode.W;
+	public KeyCode p1_down = KeyCode.S;
+	public KeyCode p1_left = KeyCode.A;
+	public KeyCode p1_right = KeyCode.D;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -15,6 +21,8 @@ public class PlayerMove : NetworkBehaviour {
 	}
 
 	void FixedUpdate () {
-		GetComponent<Rigidbody2D> ().AddForce (Vector2.right);
+		if (Input.GetKeyDown (p1_up)) {
+//			GetComponent<Rigidbody2D>().MovePosition(Vector2.MoveTowards(trans
+		}
 	}
 }
