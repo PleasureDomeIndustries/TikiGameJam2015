bb2bad076ffb5f56d65eebb81f15c7109dcba37b
whsanders <whsanders@gmail.com>
2015-08-01 11:35:56 -0400

    Empty player movement script

---
+++ TikiGame/Assets/PlayerMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerMove : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D> ().AddForce (Vector2.right);
+	}
+}
