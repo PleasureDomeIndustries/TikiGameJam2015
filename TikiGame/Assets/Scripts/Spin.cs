ee7175046041747d59e3245c33ce507bd6636077
polartb <chris@dodgersden.com>
2015-08-01 14:23:30 -0400

    Spinning Axes who collide!

---
+++ TikiGame/Assets/Scripts/Spin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    public float rotation = -10;
+
+    void Update()
+    {
+        var rigid = GetComponent<Rigidbody2D>();
+        if (rigid.velocity.magnitude > 0)         transform.Rotate(Vector3.forward * rotation);
+    }
+}
